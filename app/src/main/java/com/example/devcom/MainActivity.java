5f70eca90b3e3089fd29ee6057047be9a570196f
sosankoo <pandey.sankalp142000@gmail.com>
2020-10-13 10:43:07 +0530

    inital comment

---
+++ app/src/main/java/com/example/devcom/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.devcom;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.icu.text.IDNA;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.firebasedemo.Information;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private Button logout;
+    private EditText edit;
+    private Button add;
+    private ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logout);
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
+        listView = findViewById(R.id.listview);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_name = edit.getText().toString();
+                if (txt_name.isEmpty()){
+                    Toast.makeText(MainActivity.this, "No name entered!", Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseDatabase.getInstance().getReference().child("users").push().child("Name").setValue(txt_name);
+                }
+            }
+        });
+       final ArrayList<String> list = new ArrayList<>();
+       final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item , list);
+       listView.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                list.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Information info = snapshot.getValue(Information.class);
+                    String txt = info.getName() +" : " + info.getBranch();
+                    list.add(txt);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+            }
+
+}
